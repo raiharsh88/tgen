e1b3aeb661a91e726bf0f5520a90402745672228
raiharsh88 <raiharsh88@gmail.com>
2024-04-14 15:06:07 +0530

    cleaned up code splitter

--- backend/src/extractor/custom-rules/index.ts
+++
@@ -1,8 +0,0 @@
-import noUnusedClassMember from "./no-unused-class-member";
-import {Rule} from "eslint";
-
-
-export const rules =  {
-        ['no-unused-class-member']:noUnusedClassMember
-        // our rules will go here
-    }
--- backend/src/extractor/plugins/no-unused-class-member-plugin.ts
+++
@@ -1,4 +0,0 @@
-import noUnusedClassMember from "../custom-rules/no-unused-class-member";
-
-const noUnusedClassMemberPlugin = { rules: { "no-unused-class-member": noUnusedClassMember } };
-export default noUnusedClassMemberPlugin 
